d490105eecfbacf6d68061c42a2f278ebf332bc5
laispace <qingsheng.lai@gmail.com>
2016-08-15 18:08:52 +0800

    init

--- config.example.js
+++ config.example.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://academy.qianxiapay.com',
+        url: 'http://my-ghost-blog.com',
         mail: {},
         database: {
             client: 'sqlite3',
